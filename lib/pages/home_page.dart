d68665e200590bdc8995600b9f1ed27581de636e
BarkaBoss <bossmajor.biii@gmail.com>
2023-01-19 21:45:58 +0100

    Displaying Coin Rates from API

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:coin_cap/services/http_service.dart';
 import 'package:coin_cap/widgets/custom_dropdown.dart';
 import 'package:flutter/material.dart';
@@ -24,19 +26,74 @@ class _HomePageState extends State<HomePage>{
     _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _selectedCoin(
-              ["Bitcoin", "Ethereum"], _deviceWidth!),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _selectedCoin(
+                ["Bitcoin", "Ethereum"], _deviceWidth!),
+              _dataWidget(),
+            ],
+          ),
         ),
       ),
     );
   }
   Widget _selectedCoin(List<String> coins, double deviceWidth){
     return CustomDropDown(coins: coins, width: deviceWidth);
+  }
+
+  Widget _dataWidget(){
+    return FutureBuilder(
+      future: _httService!.get("/coins/bitcoin"),
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        if(snapshot.hasData){
+          Map data = jsonDecode(snapshot.data.toString());
+          num ngnPrice = data["market_data"]["current_price"]["ngn"];
+          num change24h = data["market_data"]["price_change_percentage_24h"];
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _coinImage(data["image"]["large"]),
+              _currentPrice(ngnPrice),
+              _percentageChangeWidget(change24h),
+
+            ],
+          );
+        }else{
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          );
+        }
+
+      },
+    );
+  }
+
+  Widget _currentPrice(num rate){
+    return Text("${rate.toStringAsFixed(2)} Naira",
+      style: const TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w400),
+    );
+  }
+
+  Widget _percentageChangeWidget(num change){
+    return Text("Change ${change.toString()}% in 24h",
+      style: const TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w300),
+    );
+  }
+
+  Widget _coinImage(String url){
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: _deviceHeight! * 0.02),
+      height: _deviceHeight! * 0.15,
+      width: _deviceWidth! * 0.15,
+      decoration: BoxDecoration(image: DecorationImage(image: NetworkImage(url)))
+    );
   }
 }
--- lib/widgets/custom_dropdown.dart
+++ lib/widgets/custom_dropdown.dart
@@ -9,18 +9,22 @@ class CustomDropDown extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: width * 0.1),
-      width: width,
+      padding: EdgeInsets.symmetric(horizontal: width * 0.05),
+      width: width*0.5,
       decoration: BoxDecoration(
-          color: Colors.indigo, borderRadius: BorderRadius.circular(5)),
+          color: Colors.indigo, borderRadius: BorderRadius.circular(10)),
       child: DropdownButton(
+        dropdownColor: Colors.indigo,
         value: coins.first,
         underline: Container(),
+        icon: const Icon(Icons.arrow_drop_down_sharp),
+        iconSize: 30,
+        iconEnabledColor: Colors.white,
         elevation: 5,
         items: coins.map((e) {
           return DropdownMenuItem(
             value: e,
-            child: Text(e),
+            child: Text(e, style: const TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.w800),),
           );
         }).toList(),
         onChanged: (_) {},
